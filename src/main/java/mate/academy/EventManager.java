da6db641c3f3401ad628e99187c6b83ae4c75078
Yaroslav Pryshchepa <yrikpa200022@gmail.com>
2025-10-06 16:18:49 +0300

    Implemented a thread-safe EventManager

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,19 +1,29 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+public class EventManager {
+    private final List<EventListener> listenerList = new CopyOnWriteArrayList<>();
+    private final ExecutorService executorService = Executors.newCachedThreadPool();
+
+    public void registerListener(EventListener listener) {
+        listenerList.add(listener);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        listenerList.remove(listener);
     }
 
     public void notifyEvent(Event event) {
-
+        for (EventListener listener: listenerList) {
+            executorService.submit(() -> listener.onEvent(event));
+        }
     }
 
     public void shutdown() {
-
+        executorService.shutdown();
     }
 }
